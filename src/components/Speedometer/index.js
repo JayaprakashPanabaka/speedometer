ce28e2966e0356cf3b7bc050a88e30902b80c146
JayaprakashPanabaka <jpanabaka1729@gmail.com>
2023-07-01 06:40:21 +0000

    SUCCESS

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -5,15 +5,24 @@ import './index.css'
 class Speedometer extends Component {
   state = {speed: 0}
 
-  render() {
+  onClickApplyBrakeButton = () => {
     const {speed} = this.state
 
-    // onAccelerate = () => {
-    //   this.setState(prevState => ({speed: prevState.speed + 10}))
-    // }
-    // onBrake = () => {
-    //   this.setState(prevState => ({speed: prevState.speed - 10}))
-    // }
+    if (speed > 0) {
+      this.setState(prevState => ({speed: prevState.speed - 10}))
+    }
+  }
+
+  onClickAccelerateButton = () => {
+    const {speed} = this.state
+
+    if (speed < 200) {
+      this.setState(prevState => ({speed: prevState.speed + 10}))
+    }
+  }
+
+  render() {
+    const {speed} = this.state
 
     return (
       <div className="bg-container">
@@ -22,16 +31,23 @@ class Speedometer extends Component {
           className="image"
           src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
           alt="speedometer"
-          //   style="max-width:70%;box-shadow:0 2.8px 2.2px rgba(0, 0, 0, 0.12) "
         />
         <div className="main-container">
           <h1>Speed is {speed}mph</h1>
           <p className="para">Min limit is 0mph, Max limit is 200mph</p>
           <div className="btn-container">
-            <button onClick={this.onAccelerate} className="accelerate">
+            <button
+              type="button"
+              onClick={this.onClickAccelerateButton}
+              className="accelerate"
+            >
               Accelerate
             </button>
-            <button onClick={this.onBrake} className="brakes">
+            <button
+              type="button"
+              onClick={this.onClickApplyBrakeButton}
+              className="brakes"
+            >
               Apply Brake
             </button>
           </div>
